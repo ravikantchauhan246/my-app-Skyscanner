b41e7accdf2de69f9ed659fc4418a41e140267c5
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2023-12-10 22:19:05 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -9,77 +9,102 @@ import format from 'date-fns/format';
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 import STYLES from './App.scss';
 
-const getClassName = cssModules(STYLES);
-
-const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
-const formatMonth = (date) => format(date, 'MMMM yyyy');
-
+const c = className => STYLES[className] || 'UNKNOWN';
+const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = date => format(date, 'MMMM yyyy');
 const daysOfWeek = [
-  { name: 'Sunday', nameAbbr: 'Sun', index: 0, isWeekend: true },
-  { name: 'Monday', nameAbbr: 'Mon', index: 2, isWeekend: false },
-  { name: 'Tuesday', nameAbbr: 'Tue', index: 0, isWeekend: false },
-  { name: 'Wednesday', nameAbbr: 'Wed', index: 0, isWeekend: false },
-  { name: 'Thursday', nameAbbr: 'Thur', index: 0, isWeekend: false },
-  { name: 'Friday', nameAbbr: 'Fri', index: 0, isWeekend: false },
-  { name: 'Saturday', nameAbbr: 'Sat', index: 0, isWeekend: true },
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tues',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thur',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    index: 6,
+    isWeekend: true,
+  },
 ];
 
-class App extends Component {
-  constructor() {
+
+export default class App extends Component {
+  constructor () {
     super();
 
     this.state = {
       selectionConfiguration: {
         type: CALENDAR_SELECTION_TYPE.single,
         date: null,
-      },
+      }
     };
   }
 
   handleDateSelect = (date) => {
     this.setState({
       selectionConfiguration: {
-        type: this.props.selectionConfiguration.type,
+        type: CALENDAR_SELECTION_TYPE.single,
         date: date,
       },
     });
-  };
+  }
 
-  render() {
+  render () {
     return (
-      <div className={getClassName('App')}>
-        <header className={getClassName('App__header')}>
-          <div className={getClassName('App__header-inner')}>
-            <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>
-              Flight Schedule
-            </BpkText>
-          </div>
-        </header>
-        <main className={getClassName('App__main')}>
-          <BpkInput
-            id="dateInput"
-            type={INPUT_TYPES.text}
-            name="date"
-            value={(this.state.selectionConfiguration.date || '').toString()}
-            placeholder="Departure date"
-          />
+      <div className={c('App')}>
+      <header className={c('App__header')}>
+        <div className={c('App__header-inner')}>
+          <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Reservation Date</BpkText>
+        </div>
+      </header>
+      <main className={c('App__main')}>
+        <div>
           <BpkCalendar
-            id="calendar"
+            id='calendar'
             onDateSelect={this.handleDateSelect}
             formatMonth={formatMonth}
             formatDateFull={formatDateFull}
             daysOfWeek={daysOfWeek}
-            weekStartsOn={1}
+            weekStartsOn={0}
             changeMonthLabel="Change month"
             nextMonthLabel="Next month"
             previousMonthLabel="Previous month"
             selectionConfiguration={this.state.selectionConfiguration}
           />
-          <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
-        </main>
-      </div>
-    );
+          </div>
+        <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+      </main>
+    </div>
+    )
   }
 }
-
-export default App;
